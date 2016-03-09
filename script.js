397dbb2704dfc751239acb7dc753de17f7cdca63
Simon PREVIDENTE <simon.previdente@free.fr>
2016-03-09 11:06:14 +0100

    Graphic-bar with CSS

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 (function() {
+  var dataset = [];
+  for (var i = 0; i < 10; i++) {
+      var newNumber = Math.floor((Math.random() * 30) + 2);
+      dataset.push(newNumber);
+  }
+
+  d3.select("body").select("#graphic-bar").selectAll("div")
+    .data(dataset)
+    .enter()
+    .append("div")
+    .attr("class", "bar")
+    .append("div")
+    .attr("class", "filling")
+    .style("height", function(d) { return d/Math.max(...dataset)*100 + "%"; })
+    .text(function(d) { return d; });
 
 })();
