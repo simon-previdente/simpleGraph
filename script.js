8e12060f1fb30df80d40c532f916d0f569c029d9
Simon PREVIDENTE <simon.previdente@free.fr>
2016-03-09 16:47:16 +0100

    Graphic-bar with SVG

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 (function() {
+  /* Data */
+  var nbValues = 10;
   var dataset = [];
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < nbValues; i++) {
       var newNumber = Math.floor((Math.random() * 30) + 2);
       dataset.push(newNumber);
   }
+  var maxValue = Math.max(...dataset);
 
-  d3.select("body").select("#graphic-bar").selectAll("div")
-    .data(dataset)
-    .enter()
-    .append("div")
-    .attr("class", "bar")
-    .append("div")
-    .attr("class", "filling")
-    .style("height", function(d) { return d/Math.max(...dataset)*100 + "%"; })
-    .text(function(d) { return d; });
+  /* SVG properties */
+  var width = 400;
+  var height = 400;
 
+  /* graphic bar */
+  var barPadding = 6;
+  var gbSvg = d3.select("body").select("#graphic-bar")
+                .attr("width", width)
+                .attr("height", height);
+  var gbBars = gbSvg.selectAll("rect")
+                     .data(dataset)
+                     .enter()
+                     .append("rect");
+  gbBars.attr("width", (width / nbValues) - barPadding)
+        .attr("height", function(d) { return d * (height / maxValue) })
+        .attr("x", function(d, i) { return i * ( this.width.baseVal.value + barPadding ) + (barPadding/2); })
+        .attr("y", function(d) { return height - this.height.baseVal.value });
+  var gbLabels = gbSvg.selectAll("text")
+                      .data(dataset)
+                      .enter()
+                      .append("text");
+  gbLabels.text(function(d) { return d; })
+          .attr("text-anchor", "middle")
+          .attr("x", function(d, i) { return i * width/nbValues + 0.5*width/nbValues; })
+          .attr("y", height - 3);
 })();
